00e68965ad4cc7fd1171132dfb8c8932f4744826
enze <enzewei@gmail.com>
2018-03-15 15:04:01 +0800

    测试

---
+++ Exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * XBsoft Exception package
+ *
+ * @category php
+ * @package xbsoft exception
+ * @author enze.wei <[enzewei@gmail.com]>
+ * @copyright 2018 xbsoft
+ * @version Develop 1.0.0
+ * @link http://docs.tech.xbsoft.net
+ */
+namespace xb\exception;
+
+class Exception extends \Exception {
+	
+} 
